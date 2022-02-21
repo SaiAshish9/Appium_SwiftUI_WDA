cdffd9efefe0194ea6ca16657c5e76fd5dea8ec7
saiashish9 <saiashish3760@gmail.com>
2022-02-22 02:19:37 +0530

    implemented

---
+++ appiumTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  appiumTest
+//
+//  Created by Sai Ashish Darapureddy on 21/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ appiumTest/appiumTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  appiumTestApp.swift
+//  appiumTest
+//
+//  Created by Sai Ashish Darapureddy on 21/02/22.
+//
+
+import SwiftUI
+
+@main
+struct appiumTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
